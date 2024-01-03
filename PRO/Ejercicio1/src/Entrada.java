3c5387211aeb242c9bc2f23fb09d58df70af7b00
Roberto Javier Ramos Aleman <148208280+RobertoJRA@users.noreply.github.com>
2024-01-03 10:23:35 +0100

    Cambio en el main

--- PRO/Ejercicio1/src/Entrada.java
+++ PRO/Ejercicio1/src/Entrada.java
@@ -2,5 +2,10 @@ public class Entrada {
 
     public static void main(String[] args) {
         System.out.println("Esto es un ejemplo de ejecucion");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indica tu nombre");
+        String nombre = scanner.next();
+        System.out.println("El nombre introducido es "+nombre);
+
     }
 }
